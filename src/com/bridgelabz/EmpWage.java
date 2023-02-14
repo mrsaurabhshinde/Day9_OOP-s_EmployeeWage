db86dfe14f1f15f15d7fc5a524389f9f0bb797b3
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-14 12:35:25 +0530

    added calculate wage of max hours and max days

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -2,5 +2,46 @@ package com.bridgelabz;
 
 public class EmpWage
 {
+    public static void main(String args[]) {
+        //constant
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
+        final int WAGE_PER_HR = 20;
+        final int MAX_WORKING_DAYS = 20;
+        final int MAX_WORKING_HRS = 100;// adding max working hr for month
+
+        int totalWage = 0;
+        int workingHrs = 0;
+        System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
+
+        /*
+         using for loop for day, workinghrs, wage, and totalworkinghrs
+         */
+        for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
+                && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs) {
+
+            int empType = (int) (Math.random() * 100) % 3;
+            //using switch case for full time and part time working hrs
+
+            switch (empType)
+            {
+                case FULL_TIME:
+                    workingHrs = 8;
+                    break;
+                case PART_TIME:
+                    workingHrs = 4;
+                    break;
+                default:
+                    workingHrs = 0;
+                    break;
+            }
+            //calculate daily empwage
+            int wage = workingHrs * WAGE_PER_HR;
+            totalWage += wage;//monthly emwage
+            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
+
+        }
+        System.out.println("Total wage for a month is " + totalWage+".");
+    }
 
 }
