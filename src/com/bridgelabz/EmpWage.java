df198d7e81e11246cb88c98d4fd7231bc1bb7c2f
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-14 12:15:18 +0530

    added daily wage calculation

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -2,20 +2,25 @@ package com.bridgelabz;
 
 public class EmpWage
 {
-    public static void main(String args[]) {
+    public static void main(String args[]){
 
+        //constant
         final int FULL_TIME = 1;
-
+        final int WAGE_PER_HR = 20;
         int empType = (int) (Math.random() * 100) % 2;
-
+        int workingHours = 0;
+        /*
+        if condition is true then employee is present otherwise absent
+         */
         if (empType == FULL_TIME)
         {
             System.out.println("Employee is Present");
-
+            workingHours = 8;
         } else
         {
             System.out.println("Employee is Absent");
-
         }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
